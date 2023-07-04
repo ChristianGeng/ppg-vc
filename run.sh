26d61e270c0d1be29e5be9ee0e867f937eb39460
ChristianGeng <christian.c.geng@gmail.com>
2023-07-04 13:08:12 +0200

    Avoid hardsetting gpu device.

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 . ./path.sh || exit 1;
-export CUDA_VISIBLE_DEVICES=0
 
 ########## Train BiLSTM oneshot VC model ##########
 python main.py --config ./conf/bilstm_ppg2mel_vctk_libri_oneshotvc.yaml \
